9ee20370061dc06436c50dc10346b98c93a2276c
Minvrl <130864645+Minvrl@users.noreply.github.com>
2024-01-17 01:02:36 +0400

    Add files via upload

---
+++ DAY19.Task/DAY19.Task/ChinaCity.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace DAY19.Task
+{
+    internal class ChinaCity:City
+    {
+        public new int Population
+        {
+            get
+            {
+                return base.Population;
+            }
+            set
+            {
+                if(value > 100000) 
+                    base.Population = value;    
+                
+            }
+
+        }
+
+       
+
+
+
+
+
+    }
+}
---
+++ DAY19.Task/DAY19.Task/City.cs
@@ -0,0 +1,49 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace DAY19.Task
+{
+    internal class City
+    {
+
+        private string _name;
+        private int _population;
+        public string Name {
+            get
+            {
+                return _name;
+            }
+            set 
+            { 
+                if( value.Length >3 && value.Length <20 && AllLetter(value))
+                    _name = value;
+            }
+        }
+        public int Population
+        {
+            get
+            {
+                return _population;
+            }
+            set 
+            { 
+                if(value > 0)
+                    _population = value;
+            }
+        }
+
+        public bool AllLetter(string str)
+        {
+            for (int i = 0; i < str.Length; i++)
+            {
+                if (!char.IsLetter(str[i]))
+                {
+                    return false;
+                }
+            }
+            return true;
+        }
+
+    }
+}
